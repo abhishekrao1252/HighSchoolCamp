60184bf611dad0f271cc7975cefda7fd5bc935aa
Abhishek Rao <abhishekrao1252@gmail.com>
2019-06-12 11:48:41 -0500

    adding fucntion exersizes

---
+++ function_practice.py
@@ -0,0 +1,22 @@
+"""
+title: function_practice
+author: Abhishek Rao
+date: 2019-06-12 11:23
+"""
+
+import math
+
+def age_calcualtor(x, y):
+    return abs(x-y)
+
+def avg_numbers(x, y, z):
+    return((x+y+z)/3)
+
+print(age_calcualtor(3244, 1348))
+print(avg_numbers(2, 5, 1))
+
+
+
+
+
+
